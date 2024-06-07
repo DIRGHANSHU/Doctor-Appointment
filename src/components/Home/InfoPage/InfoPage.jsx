444d01895ef1b06c029806c337b128fdeabf554a
Dirghanshu Patel <dirghanshup@gmail.com>
2024-06-07 10:55:24 +0530

    a

--- src/components/Home/InfoPage/InfoPage.jsx
+++ src/components/Home/InfoPage/InfoPage.jsx
@@ -13,7 +13,7 @@ const InfoPage = () => {
                         <div className="content">
                             <h3>Why Choose Us?</h3>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                                Lorem ipsum  sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
                                 Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
                             </p>
                             <div className="text-center">
