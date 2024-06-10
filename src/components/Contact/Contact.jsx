dfdb537867055f2b4a9d12e23917973db115d181
Dirghanshu Patel <dirghanshup@gmail.com>
2024-06-10 17:48:35 +0530

    d

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                                     <FaLocationArrow className='icon' />
                                     <div>
                                         <h4>Location:</h4>
-                                        <p>1212 Dhaka, Kazi Nozrul Avenur, Sylhet, Bangladesh 03214</p>
+                                        <p>1212 Dhaka, Kazi Nozrul Avenur, , Bangladesh 03214</p>
                                     </div>
                                 </div>
 
